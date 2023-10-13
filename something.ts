849de0d91c6998057aed880713dbbe9ef9edaa49
Jie Sern Teo <jteo@KLNB-174.local>
2023-10-13 09:21:51 +0800

    fallback false

--- something.ts
+++ something.ts
@@ -3,5 +3,5 @@ import * as core from '@actions/core';
 console.log('node worked')
 console.log(process.argv[2])
 core.setOutput('thing', 'hello')
-core.setOutput('fallback', true)
+core.setOutput('fallback', false)
 core.warning('something went wrong')
